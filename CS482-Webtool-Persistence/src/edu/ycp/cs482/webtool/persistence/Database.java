4911bb330a95d663a66e7e5a36be9ab3354903bb
Anthony Cummins <ajoshuacummins1@gmail.com>
2014-05-11 12:50:44 -0400

    Fixed Build Path Errors

--- CS482-Webtool-Persistence/src/edu/ycp/cs482/webtool/persistence/Database.java
+++ CS482-Webtool-Persistence/src/edu/ycp/cs482/webtool/persistence/Database.java
@@ -1,10 +1,10 @@
 package edu.ycp.cs482.webtool.persistence;
 
 import edu.ycp.cs482.webtool.persistence.IDatabase;
-import edu.ycp.cs482.webtool.persistence.MysqlDatabase;
+
 
 public class Database {
-	private static IDatabase aDatabase = new MysqlDatabase();
+	private static IDatabase aDatabase = new FakeDatabase();
 	
 	public static IDatabase getInstance() {
 		return aDatabase;
