bcb8fd64410f2cd5a067b02fa2427c5f31b99cca
Anthony Cummins <ajoshuacummins1@gmail.com>
2014-05-11 15:35:11 -0400

    Fixed Derby Connection and User Table initialization

--- CS482-Webtool-Persistence/src/edu/ycp/cs482/webtool/persistence/DerbyDatabase.java
+++ CS482-Webtool-Persistence/src/edu/ycp/cs482/webtool/persistence/DerbyDatabase.java
@@ -70,8 +70,8 @@ public class DerbyDatabase implements IDatabase {
 	}
 
 	private Connection connect() throws SQLException {
-		Connection conn = DriverManager.getConnection("jdbc:derby:E:/git/CS496Project/CS496_Project_Persistence/test.db;create=true"); //Josh
-		//Connection conn = DriverManager.getConnection("jdbc:derby:C:/Users/Mr_E/git/CS496Project/CS496_Project_Persistence/test.db;create=true"); //Anthony
+		Connection conn = DriverManager.getConnection("jdbc:derby:C:/Users/AJC/git/482-Webtool-Repository/CS482-Webtool-Persistence/test.db;create=true"); //Josh
+		//Connection conn = DriverManager.getConnection("jdbc:derby:PATH HERE/test.db;create=true"); //Anthony 
 		
 		// Set autocommit to false to allow multiple the execution of
 		// multiple queries/statements as part of the same transaction.
@@ -95,7 +95,7 @@ public class DerbyDatabase implements IDatabase {
 							"create table users (" +
 							"  id integer primary key not null generated always as identity," +
 							"  username varchar(20) unique," +
-							"  password varchar(20)," +
+							"  password varchar(20)" +
 							")"
 					);
 					
